fa7b33d0e0f466ebd7acc0f62c455066d3b10e6e
DPLaura <liriagame@gmail.com>
2018-01-30 11:30:54 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 *  $taxonomy is the taxonomy name you're looking for
 *  $id will be $post->ID'
 */
-function get_primaryTerm($taxonomy,$id) {
+function get_primaryTermName($taxonomy,$id) {
      $primary_term = new WPSEO_Primary_Term($taxonomy,$id); 
      $term = get_term_by('id',$primary_term->get_primary_term(),$taxonomy);
      return $term->name;     
